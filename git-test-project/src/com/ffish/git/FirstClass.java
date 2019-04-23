436056f89a52c0c279b4b12a89c9f0c21f72d498
ffish_glb <ffish_glb@qq.com>
2019-04-24 00:25:49 +0800

    本地工程添加到Git

---
+++ git-test-project/src/com/ffish/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.ffish.git;
+
+/**
+ * @Auther: shup
+ * @Date: 2019/4/24 00:12
+ * @Description: TODO
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project");
+    }
+}
